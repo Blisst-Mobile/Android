a1af3f74f0113b5795f736dac201ce23eb448d44
timothymiko <timothymiko@gmail.com>
2014-05-24 19:16:47 -0400

    Updated dialog for adding a new list

--- TaskManager/app/src/main/java/com/codeday/detroit/taskmanager/app/ui/TaskListFragment.java
+++ TaskManager/app/src/main/java/com/codeday/detroit/taskmanager/app/ui/TaskListFragment.java
@@ -4,12 +4,15 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.codeday.detroit.taskmanager.app.CDLog;
 import com.codeday.detroit.taskmanager.app.MainActivity;
@@ -70,10 +73,44 @@ public class TaskListFragment extends Fragment {
         View layout = getActivity().getLayoutInflater().inflate(R.layout.dialog_new_list, null);
 
         final EditText listName = (EditText) layout.findViewById(R.id.name);
+        final TextView cancelButton = (TextView) layout.findViewById(R.id.cancel);
+        final TextView saveButton = (TextView) layout.findViewById(R.id.save);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (inputMethodManager != null) {
+                    inputMethodManager.hideSoftInputFromWindow(listName.getWindowToken(), 0);
+                }
+                dialog.dismiss();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (inputMethodManager != null) {
+                    inputMethodManager.hideSoftInputFromWindow(listName.getWindowToken(), 0);
+                }
+                dialog.dismiss();
+            }
+        });
 
         builder = new AlertDialog.Builder(getActivity());
         builder.setView(layout);
         dialog = builder.create();
+        dialog.getWindow().getAttributes().windowAnimations = R.style.dialog_animation;
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (inputMethodManager != null) {
+                    inputMethodManager.showSoftInput(listName, InputMethodManager.SHOW_IMPLICIT);
+                }
+            }
+        });
         dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialog) {
@@ -82,8 +119,9 @@ public class TaskListFragment extends Fragment {
         });
     }
 
-    public void showNewListDialog() {
-        if ( dialog != null )
+    private void showNewListDialog() {
+        if ( dialog != null ) {
             dialog.show();
+        }
     }
 }
